8e9ce27b1149c9c347688d9078059c30e113810d
Ricardo Sanchez <nardove@yahoo.com>
2023-09-15 06:24:59 +0100

    Adjust colors, motion and add GUI controls

--- main.js
+++ main.js
@@ -4,17 +4,39 @@ import * as THREE from "three";
 import { Line2 } from "three/addons/lines/Line2.js";
 import { LineMaterial } from "three/addons/lines/LineMaterial.js";
 import { LineGeometry } from "three/addons/lines/LineGeometry.js";
-import * as GeometryUtils from "three/addons/utils/GeometryUtils.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { createNoise3D } from "simplex-noise";
+
+import Stats from "three/addons/libs/stats.module.js";
+import { GPUStatsPanel } from "three/addons/utils/GPUStatsPanel.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+
+let stats, gpuPanel;
+let gui;
 
 const noise3D = createNoise3D();
 const showHelperAxis = false;
 const isOrtho = true; // use orthographic or perspective camera
-const inc = 0.05;
-const scl = 30;
-const lineMaxHeight = 100;
-const lineWidth = 10;
+const scl = 28;
+
+// colors set by a sequence of r, g, b numbers,
+// so the first 3 are one color and so on
+const colors = [0, 0, 0, 0, 0.3, 1];
+const points = [0, 0, 0, 0, 0, 1];
+
+let lineWidth = 12;
+let lineHeight = 150;
+let noiseSpeed = 0.00003;
+let noiseIncrementX = 0.04;
+let noiseIncrementY = 0.04;
+
+const randomColour = () => {
+  colors[3] = Math.random();
+  colors[4] = Math.random();
+  colors[5] = Math.random();
+
+  lines.forEach((line) => line.geometry.setColors(colors));
+};
 
 /**
  * Given a value min/max range to another
@@ -60,14 +82,24 @@ const setup = () => {
         10000
       )
     : new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+
+  // By offsetting the camera, the line cap artefacts goes away
   // camera.position.x = isOrtho ? 300 : 0;
-  // camera.position.y = isOrtho ? -200 : 0;
+  camera.position.y = isOrtho ? 100 : 0;
   camera.position.z = isOrtho ? 500 : 2000;
 
   const controls = new OrbitControls(camera, renderer.domElement);
   document.body.appendChild(renderer.domElement);
 
   showHelperAxis && scene.add(new THREE.AxesHelper(200));
+
+  // GUI
+  stats = new Stats();
+  document.body.appendChild(stats.dom);
+
+  gpuPanel = new GPUStatsPanel(renderer.getContext());
+  stats.addPanel(gpuPanel);
+  stats.showPanel(0);
 
   return { width, height, scene, camera, renderer, controls };
 };
@@ -135,15 +167,9 @@ const initLinesGrid = () => {
 
   let noiseIndex = 0; // vertices has double the amount of indices as noise, this is to keep track the matching verctice and noise pair per line
 
-  // colors set by a sequence of r, g, b numbers,
-  // so the first 3 are one color and so on
-  const colors = [0, 0, 0.5, 0, 0.4, 1];
-
   for (let v = 0; v < vertices.length; v += 2) {
     const x = vertices[v];
     const y = vertices[v + 1];
-
-    const points = [0, 0, 0, 0, 0, 1];
 
     const material = new LineMaterial({
       color: 0xffffff,
@@ -151,10 +177,9 @@ const initLinesGrid = () => {
       linewidth: lineWidth,
       resolution: new THREE.Vector2(width, height),
       transparent: true,
-      // opacity: 1,
       blending: THREE.AdditiveBlending,
       depthTest: false,
-      sizeAttenuation: true,
+      alphaToCoverage: false,
     });
 
     const geometry = new LineGeometry();
@@ -188,6 +213,8 @@ let zoff = 0;
 const draw = () => {
   requestAnimationFrame(draw);
 
+  stats.update();
+
   let lineIndex = 0;
   let yoff = 0;
   for (let y = 0; y < rows; y++) {
@@ -198,15 +225,17 @@ const draw = () => {
       const angle = n * Math.PI;
 
       // lines[lineIndex].rotation.set(0, 0, angle);
-      lines[lineIndex].scale.z = mapNoise(n, -1, 1, 0, lineMaxHeight);
       lines[lineIndex].material.opacity = mapNoise(n, -1, 1, 0, 1);
-      pivots[lineIndex].rotation.x = angle;
+      // pivots[lineIndex].rotation.z = angle;
+      lines[lineIndex].scale.z = mapNoise(n, -1, 1, 0, lineHeight);
       pivots[lineIndex].rotation.y = angle;
+      lines[lineIndex].material.linewidth = lineWidth;
+      pivots[lineIndex].rotation.x = angle;
       lineIndex++;
-      xoff += inc / 1.5;
+      xoff += noiseIncrementX;
     }
-    yoff += inc;
-    zoff += 0.000025;
+    yoff += noiseIncrementY;
+    zoff += noiseSpeed;
   }
 
   controls.update();
@@ -214,3 +243,38 @@ const draw = () => {
   renderer.clearDepth();
 };
 draw();
+
+const setupGUI = () => {
+  gui = new GUI({ width: 310 });
+
+  const settings = {
+    "line width": 12,
+    "line height": 150,
+    "noise speed": 0.03,
+    "noise increment x": 0.4,
+    "noise increment y": 0.4,
+    "change random colour": () => randomColour(),
+  };
+
+  gui
+    .add(settings, "line width", 1, 20, 1)
+    .onChange((val) => (lineWidth = val));
+  gui
+    .add(settings, "line height", 50, 300, 1)
+    .onChange((val) => (lineHeight = val));
+
+  gui
+    .add(settings, "noise speed", 0.01, 0.5, 0.01)
+    .onChange((val) => (noiseSpeed = val / 10000));
+
+  gui
+    .add(settings, "noise increment x", 0.01, 1, 0.01)
+    .onChange((val) => (noiseIncrementX = val / 10));
+
+  gui
+    .add(settings, "noise increment y", 0.01, 1, 0.01)
+    .onChange((val) => (noiseIncrementY = val / 10));
+
+  gui.add(settings, "change random colour");
+};
+setupGUI();
